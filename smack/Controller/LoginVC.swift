40adcfc3a94abd1dbc16fba9038a6424633031ea
Bibhuranjan Bihari <bibhu065@gmail.com>
2017-12-31 15:52:53 +0530

    End of Lesson4

---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Bibhuranjan Bihari on 31/12/17.
+//  Copyright © 2017 Bibhuranjan Bihari. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ smack/Utilites/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Bibhuranjan Bihari on 31/12/17.
+//  Copyright © 2017 Bibhuranjan Bihari. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
